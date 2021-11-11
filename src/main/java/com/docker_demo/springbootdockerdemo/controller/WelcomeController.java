ecee7afad98a9f6e2b05fb6514780503cc2f3d30
ShitalJori98 <shital.jori@clairvoyantsoft.com>
2021-11-11 09:08:46 +0530

    Dockerfile added

---
+++ src/main/java/com/docker_demo/springbootdockerdemo/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.docker_demo.springbootdockerdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Spring boot Docker demo application !!";
+    }
+}
